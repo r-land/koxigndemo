2ed09ae053762e737c657fd01ceff7778e3b3057
polyfortuna <polyfortuna00@gmail.com>
2023-09-26 03:08:34 +0900

    0926 update: 프로젝트 초기설정

---
+++ src/main/java/koxign/login/service/impl/package-info.java
@@ -0,0 +1,1 @@
+package koxign.login.service.impl;
---
+++ src/main/java/koxign/login/web/LoginController.java
@@ -0,0 +1,11 @@
+package koxign.login.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+}
